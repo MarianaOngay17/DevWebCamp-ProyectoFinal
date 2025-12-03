df901b8943050b964dae5d23a96b816eee63f89a
Mariana <marianaongay.01@gmail.com>
2025-12-03 12:23:29 -0600

    BD, funcionalidad login, registro, olvide

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,7 +2,11 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Registrate en DevWebCamp</p>
 
-    <form class="formulario">
+    <?php
+        require_once __DIR__ . '/../templates/alertas.php';
+    ?>
+
+    <form class="formulario" method="POST" action="/registro">
         <div class="formulario__campo">
             <label class="formulario__label" for="nombre">Nombre</label>
             <input
@@ -11,6 +15,7 @@
                 placeholder="Tu Nombre"
                 id="nombre"
                 name="nombre"
+                value="<?php echo $usuario->nombre; ?>"
             />
         </div>
         <div class="formulario__campo">
@@ -21,6 +26,7 @@
                 placeholder="Tu Apellido"
                 id="apellido"
                 name="apellido"
+                value="<?php echo $usuario->apellido; ?>"
             />
         </div>
         <div class="formulario__campo">
@@ -31,6 +37,7 @@
                 placeholder="Tu Email"
                 id="email"
                 name="email"
+                value="<?php echo $usuario->email; ?>"
             />
         </div>
         <div class="formulario__campo">
